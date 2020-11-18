b6964cef7ab9d9aa93340c4f0b6dd9a43d369587
IZVARYCHEV <74112730+IZVARYCHEV@users.noreply.github.com>
2020-11-19 00:11:54 +0500

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'book-sky',
     environment,
-    rootURL: '/',
+    rootURL: '/booksky',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
