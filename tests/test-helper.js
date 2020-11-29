e5108a986fbd1c1da014560bb959e778ba81ca90
IZVARYCHEV <Ivan.zvarychev@mail.ru>
2020-11-29 19:32:42 +0500

    29.11 commit

--- tests/test-helper.js
+++
@@ -1,8 +0,0 @@
-import Application from 'booksky/app';
-import config from 'booksky/config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-
-setApplication(Application.create(config.APP));
-
-start();
